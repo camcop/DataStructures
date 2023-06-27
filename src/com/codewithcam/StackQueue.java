71d7c3e7642f08221c72d60c9b9319e2fcfc144f
Cameron Copland <cjcopland@gmail.com>
2023-06-27 16:23:31 +0100

    Refactor StackQueue

--- src/com/codewithcam/StackQueue.java
+++ src/com/codewithcam/StackQueue.java
@@ -10,25 +10,24 @@ public class StackQueue {
     }
 
     public int dequeue() {
-        if (stack1.isEmpty() && stack2.isEmpty()) throw new IllegalStateException();
-
-        if (stack2.isEmpty())
-            while (!stack1.isEmpty())
-                stack2.push(stack1.pop());
+        if (isEmpty()) throw new IllegalStateException();
+        moveStack1toStack2();
         return stack2.pop();
     }
 
-    public boolean isEmpty() {
-        return stack1.isEmpty();
+    private void moveStack1toStack2() {
+        if (stack2.isEmpty())
+            while (!stack1.isEmpty())
+                stack2.push(stack1.pop());
     }
 
-//    public boolean isFull() {
-//        return stack1.;
-//    }
+    public boolean isEmpty() {
+        return (stack1.isEmpty() && stack2.isEmpty());
+    }
 
     public int peek() {
-        while (!stack1.isEmpty())
-            stack2.push(stack1.pop());
+        if (isEmpty()) throw new IllegalStateException();
+        moveStack1toStack2();
         return stack2.peek();
     }
 
