4dc38928224865716f99b482f591b5c0395e589d
Cameron Copland <cjcopland@gmail.com>
2023-06-30 11:44:00 +0100

    Refactor StackWithTwoQueues class to only have one queue populated

--- src/com/codewithcam/StackWithTwoQueues.java
+++ src/com/codewithcam/StackWithTwoQueues.java
@@ -9,41 +9,34 @@ public class StackWithTwoQueues {
     private Queue<Integer> queue2 = new ArrayDeque<>();
 
     public void push(Integer value) {
-        if (queue1.isEmpty()) {
-            queue1.add(value);
-            while (!queue2.isEmpty()) {
-                queue1.add(queue2.remove());
-            }
-        } else {
-            queue2.add(value);
-            while (!queue1.isEmpty()) {
-                queue2.add(queue1.remove());
-            }
+        queue2.add(value);
+        while (!queue1.isEmpty()) {
+            queue2.add(queue1.remove());
         }
+        swapQueues();
+    }
+
+    private void swapQueues() {
+        while (!queue2.isEmpty())
+            queue1.add(queue2.remove());
     }
 
     public Integer pop() {
         if (isEmpty()) throw new IllegalStateException();
-        if (queue1.isEmpty())
-            return queue2.remove();
-        else
-            return queue1.remove();
+        return queue1.remove();
     }
 
     public Integer peek() {
         if (isEmpty()) throw new IllegalStateException();
-        if (queue1.isEmpty())
-            return queue2.peek();
-        else
-            return queue1.peek();
+        return queue1.peek();
     }
 
     public Integer size() {
-        return queue1.size() + queue2.size();
+        return queue1.size();
     }
 
     public boolean isEmpty() {
-        return (queue1.isEmpty() && queue2.isEmpty());
+        return queue1.isEmpty();
     }
 
 
