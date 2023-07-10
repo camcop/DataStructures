1d5f3dec80f2790ea04a3180b40eb3d44636639c
Cameron Copland <cjcopland@gmail.com>
2023-07-10 22:30:28 +0100

    Refactor AVLTree insert method

--- src/com/codewithcam/AVLTree.java
+++ src/com/codewithcam/AVLTree.java
@@ -16,30 +16,20 @@ public class AVLTree {
     private AVLNode root;
 
     public void insert(int value) {
-        if (root == null) {
-            root = new AVLNode(value);
-        }
-        insert(value, root);
-
+        root = insert(value, root);
     }
 
-    private void insert(int value, AVLNode node) {
+    private AVLNode insert(int value, AVLNode node) {
 
-        AVLNode newNode = new AVLNode(value);
+        if (node == null)
+            return new AVLNode(value);
 
-        if (value < node.value && node.leftChild == null) {
-            node.leftChild = newNode;
-            return;
-        }
-        if (value > node.value && node.rightChild == null) {
-            node.rightChild = newNode;
-            return;
-        }
         if (value < node.value)
-            insert(value, node.leftChild);
-        if (value > node.value)
-            insert(value, node.rightChild);
+            node.leftChild = insert(value, node.leftChild);
+        else
+            node.rightChild = insert(value, node.rightChild);
 
+        return node;
 
     }
 
